740326ad6d873fcd2f8159d843f5a72c7a2064a1
Brett Carswell <brett.carswell@gmail.com>
2010-04-13 15:15:14 +1000

    Additional tests for the ValueSerializer

--- Amanatsu.Message.Test/ValueSerializerTest.fs
+++ Amanatsu.Message.Test/ValueSerializerTest.fs
@@ -1,7 +1,7 @@
 ﻿namespace Amanatsu.Message.Test
 
 
-module public ValueSerializerTest =
+module public ValueSerializerTests =
 
 
     open Amanatsu.Message
@@ -15,16 +15,64 @@ module public ValueSerializerTest =
                 int (x - y)
         
     [<Fact>]
-    let testMe() = Assert.True(true)
-    
+    let Test_ValueSerializer_Can_Serialize_an_int() = 
+        let serializer = new ValueSerializer()
+        serializer.append(0x12345678);
+        let bytes = serializer.getBytes
+        Assert.Equal(8, Seq.length bytes)
+        
+        let expected = [| 0uy; 0uy; 0uy; 4uy; 0x12uy; 0x34uy; 0x56uy; 0x78uy |] |> Seq.ofArray
+        
+        Assert.Equal(expected, bytes)
+
     [<Fact>]
     let Test_ValueSerializer_Can_Serialize_A_Single_Byte() = 
-            let serializer = new ValueSerializer()
-            serializer.append(4uy)
-            let bytes = serializer.getBytes
-            Assert.Equal(5, Seq.length bytes)
-           
-            let expected = [| 0uy; 0uy; 0uy; 1uy; 4uy|] :> seq<byte>
-            
-            
-            Assert.Equal(expected, bytes)
+        let serializer = new ValueSerializer()
+        let b = 5uy
+        serializer.append(b)
+        let bytes = serializer.getBytes
+        Assert.Equal(5, Seq.length bytes)
+       
+        let expected = [| 0uy; 0uy; 0uy; 1uy; b|] |> Seq.ofArray
+        
+        Assert.Equal(expected, bytes)
+
+    [<Fact>]
+    let Test_ValueSerializer_Can_Serialize_A_String() = 
+        let serializer = new ValueSerializer()
+        
+        serializer.append("hello")
+        let bytes = serializer.getBytes
+        Assert.Equal(13, Seq.length bytes)
+       
+        let expected = [| 0uy; 0uy; 0uy; 9uy; 0uy; 0uy; 0uy; 5uy; (byte 'h'); (byte 'e'); (byte 'l'); (byte 'l'); (byte 'o') |] |> Seq.ofArray
+        
+        Assert.Equal(expected, bytes)
+
+
+    [<Fact>]
+    let Test_ValueSerializer_Can_Serialize_Multiple_Values() = 
+        let serializer = new ValueSerializer()
+        
+        serializer.append("hello")
+        serializer.append(0x00aa5500)
+        serializer.append(0x01uy)
+        let bytes = serializer.getBytes
+        Assert.Equal(18, Seq.length bytes)
+       
+        let expected = [| 0uy; 0uy; 0uy; 14uy; 0uy; 0uy; 0uy; 5uy; (byte 'h'); (byte 'e'); (byte 'l'); (byte 'l'); (byte 'o'); 0x00uy; 0xaauy; 0x55uy; 0x00uy; 0x01uy |] |> Seq.ofArray
+        
+        Assert.Equal(expected, bytes)
+        
+    [<Fact>]
+    let Test_ValueSerializer_getBytes_works_when_no_data_is_appended() = 
+        let serializer = new ValueSerializer()
+        
+        let bytes = serializer.getBytes
+        Assert.Equal(4, Seq.length bytes)
+       
+        let expected = [| 0uy; 0uy; 0uy; 0uy; |] |> Seq.ofArray
+        
+        Assert.Equal(expected, bytes)
+
+    
